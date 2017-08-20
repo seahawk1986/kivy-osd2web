e2ee3bf81c8821e6bbc0e3c577c4a170a7cf010c
Alexander Grothe <seahawk1986@gmx.de>
2017-08-20 15:35:16 +0200

    Add broadcast_message function to allow external callers to send data

--- websocket.py
+++ websocket.py
@@ -1,4 +1,5 @@
 import json
+from twisted.internet import reactor
 from autobahn.twisted.websocket import WebSocketClientFactory, \
                                        WebSocketClientProtocol
 from twisted.internet.protocol import ReconnectingClientFactory
@@ -15,30 +16,40 @@ LOGOUT = json.dumps(
         }).encode('utf-8')
 
 class CustomWebsocketClientProtocol(WebSocketClientProtocol):
+    connections = list()
+    
+    def onConnect(self, response):
+        print("Server connected: {0}".format(response.peer))
+        self.connections.append(self)
+        self.factory.resetDelay()
 
-   def onConnect(self, response):
-       print("Server connected: {0}".format(response.peer))
-       self.factory.resetDelay()
-
-   def onOpen(self):
+    def onOpen(self):
        self.sendMessage(LOGIN)
 
-   def onClose(self):
-       self.sendMessage(LOGOUT)
+    def onMessage(self, payload, isBinary):
+        if isBinary:
+            #print("Binary message received: {0} bytes".format(len(payload)))
+            pass
+        else:
+            data = json.loads(payload.decode('utf-8'))
+            name = data['event']
+            dat = data['object']
+            self.app.update_data(name, dat)
 
-   def onMessage(self, payload, isBinary):
-      if isBinary:
-         #print("Binary message received: {0} bytes".format(len(payload)))
-         pass
-      else:
-         data = json.loads(payload.decode('utf-8'))
-         name = data['event']
-         dat = data['object']
-         self.app.update_data(name, dat)
+    def onClose(self, wasClean, code, reason):
+        self.connections.remove(self)
+        try:
+           self.sendMessage(LOGOUT)
+        except Exception as e:
+           print("failed to send LOGOUT:", e)
+        print("connection closed")
+        print(wasClean, code, reason)
 
-   def onClose(self, wasClean, code, reason):
-       print("connection closed")
-       print(wasClean, code, reason)
+    @classmethod
+    def broadcast_message(cls, data):
+        payload = json.dumps(data, ensure_ascii = False).encode('utf8')
+        for c in set(cls.connections):
+            reactor.callFromThread(cls.sendMessage, c, payload)
 
 
 class WSClientFactory(WebSocketClientFactory, ReconnectingClientFactory):
